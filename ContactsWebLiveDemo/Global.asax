bcc81325708ab7996c275c702f5b41aba4783428
Mikhail Goodridge <mikhailgoodridge@outlook.com>
2022-09-08 21:38:36 -0400

    Add project files.

---
+++ ContactsWebLiveDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ContactsWebLiveDemo.MvcApplication" Language="C#" %>
---
+++ ContactsWebLiveDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContactsWebLiveDemo.Startup))]
+namespace ContactsWebLiveDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
